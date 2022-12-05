6d2f562409f10b6bb4b79fe0f25a7c838cbd9716
amos <amosjshinkle@gmail.com>
2022-12-05 13:56:10 -0500

    commit tree 804c099489704d37fe92946a6ff5fe0141c22a07

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+
+
+function findMatching(drivers, name) {
+    let names = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return names
+}
+
+function fuzzyMatch(drivers, letters) {
+    let names = drivers.filter(driver => driver.startsWith(letters))
+    return names
+}
+
+function matchName(drivers, driverName) {
+    let names = drivers.filter(driver => driver.name === driverName)
+    return names
+}
